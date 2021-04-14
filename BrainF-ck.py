3f52b7908342955cd2e1c5e982a3fdd0a5e0e191
Fukuda-B <60131202+Fukuda-B@users.noreply.github.com>
2021-04-14 11:37:10 +0900

    Update BrainF-ck.py

--- BrainF-ck.py
+++ BrainF-ck.py
@@ -12,7 +12,8 @@ import re
 
 class BrainFuck():
 
-    def bf(self, tx:str):
+    # コンパイラ本体
+    async def bf(self, tx:str):
         """Exec BrainF*ck"""
         shift = 0
         maxShift = 0
@@ -21,27 +22,40 @@ class BrainFuck():
         Error = ""
         for i in range(len(tx)):
             vs = str(tx[i])
-            if vs == '+':
+            if vs == '+': # インクリメント
                 arr[shift] += 1
-            elif vs == '-':
+            elif vs == '-': # デクリメント
                 arr[shift] -= 1
-            elif vs == '>':
+            elif vs == '>': # ポインタシフト (+)
                 if len(arr) <= shift+1: arr.append()
                 shift += 1
                 if shift > maxShift : maxShift = shift
-            elif vs == '<':
+            elif vs == '<': # ポインタシフト(-)
                 shift -= 1
                 if shift < 0:
                     Error = "Out of range of array Error"
                     break
             elif vs == '.':
                 out.append(arr[shift])
-            # elif vs == '[':
+            elif vs == '[':
+                if arr[shift] != 0:
+                    c_cnt, res_loop = 1, [] # ループチェッカー, 戻り値
+                    s_ptr, e_ptr = sfhit, 0 # 開始ポインタ, 終了ポインタ
+                    l_cnt = 0 # ループカウンタ
+                    while(c_cnt == 0):
+                        if vvs == '[': cnt += 1
+                        if vvs == ']': cnt -= 1
+                        l_cnt += 1
+                    e_ptr = shift + l_cnt
+
             # elif vs == ',':
 
         result = out
         return result, Error
 
+    # コメント削除
+    async def origin(tx):
+        return ''.join(filter(lambda x: x in ['.', ',', '[', ']', '>', '<', '+', '-'], tx))
 
 # ----- Exec
 
