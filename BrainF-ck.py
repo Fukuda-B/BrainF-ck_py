e150f078ec293319a1fbf86bce1d7a0b3d72b2a6
Fukuda-B <60131202+Fukuda-B@users.noreply.github.com>
2021-05-03 19:36:40 +0900

    remaking

--- BrainF-ck.py
+++ BrainF-ck.py
@@ -22,83 +22,66 @@ _INPUT = """\
 sys.stdin = io.StringIO(_INPUT)
 
 # ----- Main
-# import re
-
-# ----- Main
 class BrainFuck():
-
-    # self, ソースコード, デバッグオプション
-    def bf(self, tx:str, option):
-        """Exec BrainF*ck"""
-        tx = BrainFuck.origin(self, tx) # コメントなしのコード
-        shift = 0 # ポインタ保持変数
-        maxShift = 0
-        arr = [0]
-        out = [] # 出力
-        Error = ""
-        i = 0 # コードの解析位置
-        debug = "" # ステップデバッグ
-        while 1:
-
-            if option == 1: debug += str(tx[i])+" @"+str(shift)+" "+str(arr)+"\n" # Debug
     
+    def __init__(self, code, option):
+        self.parsed = "" # parsed code
+        self.code = BrainFuck.origin(self, code) # no comment code
+        self.option = option # debug option
+        self.out = [] # output string
+        self.error = "" # output error
+        self.arr = [0] # memory value
+        self.maxShift = 0 # max pointer position
+        self.shift = 0 # pointer position
+        self.i = 0 # counter
+        self.debug = [] # debug result
+
+    def bf_main(self, code:str):
+        """ Exec BrainF*ck """
+        i, tx, arr, shift = \
+            0, code, self.arr, self.shift
+        while 1:
+            if self.option: self.debug.append(f'{tx[i]}" @{self.shift} {self.arr}"') # debug
+            if not tx[i] == '[' and not tx[i] == ']': self.parsed += tx[i]
+
             vs = str(tx[i])
-            if vs == '+': # 255未満ならインクリメント、255以上なら0
-                arr[shift] = (arr[shift]+1) if arr[shift] < 255 else 0
-            elif vs == '-': # 0より大きい場合はデクリメント、0未満なら255
-                arr[shift] = (arr[shift]-1) if arr[shift] > 0 else 255
+            if vs == '+': self.arr[self.shift] = (self.arr[self.shift]+1) if self.arr[self.shift] < 255 else 0 # 255未満ならインクリメント、255以上なら0
+            elif vs == '-': self.arr[self.shift] = (self.arr[self.shift]-1) if self.arr[self.shift] > 0 else 255 # 0より大きい場合はデクリメント、0未満なら255
             elif vs == '>': # ポインタをインクリメント (+)
-                if len(arr) <= shift+1: arr.append(0)
-                shift += 1
-                if shift > maxShift : maxShift = shift
+                if len(self.arr) <= self.shift+1: self.arr.append(0)
+                self.shift += 1
+                if self.shift > self.maxShift: self.maxShift = self.shift
             elif vs == '<': # ポインタをデクリメント(-)
-                shift -= 1
-                if shift < 0:
-                    Error = "Out of range of array Error"
+                self.shift -= 1
+                if self.shift < 0:
+                    self.error = 'Out of range of array Error'
                     break
             elif vs == '.': # 出力の追加
-                out.append(arr[shift])
+                self.out.append(self.arr[self.shift])
             elif vs == '[':
-                if arr[shift] != 0:
-                    c_cnt, res_loop = 0, "" # ループチェッカー, ループの展開した結果
-                    s_ptr, e_ptr = i, 0 # 開始ポインタ, 終了ポインタ
-                    l_cnt = 0 # ループカウンタ
-                    while(c_cnt != 0 or l_cnt == 0):
-                        vvs = tx[i+l_cnt] # コード解析用
-                        if vvs == '[': c_cnt += 1
-                        if vvs == ']': c_cnt -= 1
-                        l_cnt += 1
-                        if i+l_cnt > len(tx):
-                            Error = "Corresponding brackets are missing."
-                            break
-                    e_ptr = i + l_cnt
-                    if e_ptr > 0:
-                        for j in range(arr[shift]):
-                            res_loop += tx[(i+1):(e_ptr-1)]
-                    tx = str(tx[:(s_ptr)]) + str(res_loop) + str(tx[(e_ptr):]) # コード展開
-                    i -= 1 # コード展開により "[" 1命令文減るため、戻す
-                else: # "[" かつポインタの指す値が0だった場合
-                    while 1:
-                        i += 1
-                        if tx[i] == ']': break
-                        if i > len(tx):
-                            Error = "Corresponding brackets are missing."
-                            break
+                c_cnt, l_cnt = 1, 1
+                while(c_cnt != 0):
+                    vvs = tx[i+l_cnt]
+                    if vvs == '[': c_cnt += 1
+                    if vvs == ']': c_cnt -= 1
+                    l_cnt += 1
+                    if i+l_cnt > len(tx):
+                        self.error = ' ] are missing.'
+                        break
+
+                while self.arr[self.shift] != 0:
+                    self.arr = BrainFuck.bf_main(self, tx[i+1:i+l_cnt-1])
+                print(l_cnt ,self.i, len(tx))
             i += 1
-            # print(tx, arr, i)
-            # print(tx[i], arr, arr[shift], "||", tx[i+1])
-            if i >= len(tx): break
-            # elif vs == ',':
+            if i >= len(tx):
+                self.i += i
+                break
+        return self.arr
 
-        # ASCII Decode
-        result = ""
-        for i in range(len(out)):
-            result += chr(out[i])
-
-        # 出力, エラー, 結果,
-        # 展開後のコード, ステップ数, ステップデバッグ
-        return result, Error, out,\
-        tx, len(tx), debug
+    # self, ソースコード, デバッグオプション
+    def bf(self):
+        """Exec BrainF*ck"""
+        res = BrainFuck.bf_main(self, self.code)
 
     # コメント削除
     def origin(self, tx):
@@ -107,6 +90,13 @@ class BrainFuck():
 # ----- Exec
 
 inputs = str(input())
-bfc = BrainFuck()
-res = bfc.bf(inputs, 1)
+bfc = BrainFuck(inputs, 1)
+bfc.bf()
+print(bfc.out)
+
+
+# -----
+if __name__ == '__main__':
+    pass
+nputs, 1)
 print(res[0])
